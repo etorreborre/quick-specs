09e9777c2a90665dfe35e50314ad693ab33a8bfa
etorreborre <etorreborre@yahoo.com>
2010-05-31 23:28:17 +1000

    started the commenting of equivalence classes

--- src/main/scala/org/specs/quick/classify/EquivalenceClass.scala
+++ src/main/scala/org/specs/quick/classify/EquivalenceClass.scala
@@ -2,12 +2,38 @@ package org.specs.quick.classify
 import org.specs.quick.expression._
 import org.specs.quick.methods._
 
+/**
+ * An equivalence class is a list of Valued Expressions which give the same result for a given set 
+ * of variables
+ */
 case class EquivalenceClass(expressions: List[ValuedExpression], variables: List[Variable[_]], result: Option[Any]) {
-  def show = expressions.map(_.show).mkString(" == ")
 
+  /** create an equivalence class with no result yet */
   def this(expressions: List[ValuedExpression], variables: List[Variable[_]]) = this(expressions, variables, None)
-  
+  /**
+   *  recursively partition this equivalence class n times
+   */
+  def partition(n: Int): List[EquivalenceClass] = { 
+    (List(this) /: (1 to n)) { (res, cur) => res.flatMap(_.partition) }
+  }
+
+  /**
+   * @return a list of EquivalenceClass where each class groups expression having the same result
+   */
+  private def partition: List[EquivalenceClass] = evaluate.map { case (result, exps) =>  
+    new EquivalenceClass(exps, variables, Some(result)) 
+  }.toList
+  /**
+   * @return a map of expressions sorted by result
+   */
+  private def evaluate: scala.collection.Map[Any, List[ValuedExpression]] = {
+	variables.foreach(_.evaluate)
+	expressions.groupBy(_.value)
+  }
+
+  def show = expressions.map(_.show).mkString(" == ")
   def equations = "Equivalence: " + result.getOrElse("None") + " -> " + show
+  
   def equalities: List[ExpressionEquality] = makeEqualities(expressions)
   private def makeEqualities(exp: List[Expression]): List[ExpressionEquality] = exp match {
 	case Nil => Nil
@@ -15,15 +41,6 @@ case class EquivalenceClass(expressions: List[ValuedExpression], variables: List
 	case e :: other :: Nil if (e != other) => List(ExpressionEquality(e, other))
 	case e :: other :: others if (e != other) => ExpressionEquality(e, other) :: makeEqualities(others)
 	case _ => Nil
-  }
-  def evaluate: scala.collection.Map[Any, List[ValuedExpression]] = {
-	variables.foreach(_.evaluate)
-	expressions.groupBy(_.value)
-  }
-   
-  private def partition: List[EquivalenceClass] =  evaluate.map { e =>  new EquivalenceClass(e._2, variables, Some(e._1)) }.toList
-  def partition(n: Int): List[EquivalenceClass] = { 
-    (List(this) /: (1 to n)) { (res, cur) => res.flatMap(_.partition) }
   }
 }
 trait Curryfiable {
--- src/main/scala/org/specs/quick/classify/ExpressionsClassifier.scala
+++ src/main/scala/org/specs/quick/classify/ExpressionsClassifier.scala
@@ -2,9 +2,15 @@ package org.specs.quick.classify
 import org.specs.quick.expression._
 import org.specs.quick.methods._
 
+/**
+ * This trait implements the classification strategy once the 
+ * methods and variables have been combined.
+ * 
+ * At the moment the strategy is very naive. For each input equivalence class, it partitions it
+ * 4 times by evaluating its expressions and dividing the expressions by distinct results
+ */
 trait ExpressionsClassifier {
   val classify = classifyExpressions _
-  private def classifyExpressions(equivalenceClass: EquivalenceClass) = {
-    equivalenceClass.partition(4)
-  }
+  
+  private def classifyExpressions(equivalenceClass: EquivalenceClass) = equivalenceClass.partition(4)
 }
