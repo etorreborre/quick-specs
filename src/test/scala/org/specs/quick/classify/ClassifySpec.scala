8f56bde2139ee16d6440e354cc92de1edd5328e5
etorreborre <etorreborre@yahoo.com>
2010-07-11 11:15:59 +1000

    fixed the ClassifySpec

--- src/test/scala/org/specs/quick/classify/ClassifySpec.scala
+++ src/test/scala/org/specs/quick/classify/ClassifySpec.scala
@@ -7,17 +7,18 @@ class ClassifySpec extends SpecificationWithJUnit with ExpressionsClassifier wit
   with SampleVariables with Sugar {
   noDetailedDiffs()
   override val partitionsNumber = 4
-  combineDepth(1)
+  combineDepth(2)
   
-  val combined = combine((plusPlusAndNil, List(xs)))
+  val combined = combine((plusPlusAndNil, List(lists, xs)))
   "classifying expressions" should {
 	"evaluate expressions to separate them to equalities" in {
-	  classify(combined).map(_.toString).mkString("\n") must_== List( 
+	  val expected = List(
 	 	  "[xs == xs]",
-	 	  "[xs == ++(nil(), xs)]",
-	 	  "[xs == ++(xs, nil())]",
-	 	  "[++(xs, xs) == ++(xs, xs)]",
-     	  "[nil() == ++(nil(), nil())]").mkString("\n")
+	 	  "[xs == lists.++(lists.nil(), xs)]",
+	 	  "[xs == lists.++(xs, lists.nil())]",
+     	  "[lists.nil() == lists.++(lists.nil(), lists.nil())]")
+	  val result = classify(combined).map(_.toString) 
+	  expected foreach (result must contain(_))
 	}  
 	"return equalities for tautologies" in {
 	  classify(combined).map(_.toString) must contain("[xs == xs]") 
