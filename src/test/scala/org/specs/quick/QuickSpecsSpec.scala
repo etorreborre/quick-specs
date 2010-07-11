9b8f19b26f0e622d68c132830088f6616d4c93c4
etorreborre <etorreborre@yahoo.com>
2010-07-12 07:25:36 +1000

    fixed the QuickSpec spec

--- src/test/scala/org/specs/quick/QuickSpecsSpec.scala
+++ src/test/scala/org/specs/quick/QuickSpecsSpec.scala
@@ -7,10 +7,13 @@ import org.specs.log._
 class QuickSpecsSpec extends SpecificationWithJUnit with QuickSpecs with Sugar with SampleLists with SampleVariables with Log {
   noDetailedDiffs()
   "creating quick specs" should {
+	val nil = constant("lists.nil()", Lists.nil())
     "return a list of equations for the most simple case" in {
       combineDepth(1)
-      val equations = quick(Lists.accept("\\+\\+", "nil"), lists, xs).split("\n")
+      val equations = quick(Lists.accept("\\+\\+"), lists, xs, nil).split("\n")
       equations must have size(2)
+      "[xs == lists.++(lists.nil(), xs)]" +
+      "[xs == lists.++(xs, lists.nil())]" 
     }
     "return a list of equations for a case with 2 variables and one operation" in {
       combineDepth(1)
@@ -20,9 +23,9 @@ class QuickSpecsSpec extends SpecificationWithJUnit with QuickSpecs with Sugar w
     }
     "return a list of equations for a case with 2 variables and two operations" in {
       combineDepth(1)
-      level = Debug
-      quick(Lists.accept("\\+\\+", "nil"), lists, xs, ys).split("\n").toList must have size(2)
-      quick(Lists.accept("\\+\\+", "nil"), lists, xs, ys).toString must include("[xs == lists.++(lists.nil(), xs)]") and include("[xs == lists.++(xs, lists.nil())]") 
+      quick(Lists.accept("\\+\\+"), lists, xs, ys, nil).split("\n").toList must have size(2)
+      quick(Lists.accept("\\+\\+"), lists, xs, ys, nil).toString must include("[xs == lists.++(lists.nil(), xs)]") and
+                  include("[xs == lists.++(xs, lists.nil())]") 
     }
   }
 }
