2d31e623940a37908764d6350bc08dda67841093
etorreborre <etorreborre@yahoo.com>
2010-07-13 08:02:51 +1000

    started the refactoring to prepare for combining functions

--- src/main/scala/org/specs/quick/methods/ScalaMethods.scala
+++ src/main/scala/org/specs/quick/methods/ScalaMethods.scala
@@ -1,14 +1,21 @@
 package org.specs.quick.methods
 import org.specs.util.Classes
+import org.specs.util.TypesMatcher
 import org.specs.specification.Tagged
 
+trait ScalaMethods {
+  /** @return the list of accepted methods */
+  def methods: List[ScalaMethod]
+}
 /**
  * This class holds a list of methods, belonging to the same class, for which equations will be generated.
  * 
  * It is tagged so that some methods can be accepted or rejected from the set by their name
  */
-class ScalaMethods(declaringClass: Class[_], private val scalaMethods: List[ScalaMethod]) extends Tagged {
-  import ScalaMethods._
+class ScalaClassMethods(declaringClass: Class[_], private val scalaMethods: List[ScalaMethod]) extends 
+  Tagged with TypesMatcher with ScalaMethods {
+  
+	import ScalaMethods._
   
   def getOwn: List[ScalaMethod] = {
     val selected = if (declaringClass.getSuperclass != null)
@@ -38,12 +45,14 @@ class ScalaMethods(declaringClass: Class[_], private val scalaMethods: List[Scal
    * get methods, adding typing information
    */
   def select(methodNames: String*) = {
-	new ScalaMethods(declaringClass, selectMethods(methodNames.toList))
+	new ScalaClassMethods(declaringClass, selectMethods(methodNames.toList))
   }
   private def selectMethods(methodNames: List[String]) = {
 	methodNames.flatMap { name =>
 	  val (selectedName, types) = extractNameAndTypes(name)
-	  scalaMethods.find(_.methodName == selectedName).map(m => m.parameterTypes = types)
+	  scalaMethods.filter(_.methodName == selectedName).find { m => 
+	    typesMatch(m.parameterTypes.drop(1), types)
+	  }.map(m => m.parameterTypes = types)
 	}
   }
 }
@@ -55,8 +64,8 @@ object ScalaMethods {
   /**
    * create a ScalaMethods object from an object
    */
-  def create[T <: AnyRef](a: T)(implicit m: ClassManifest[T]): ScalaMethods = {
-    new ScalaMethods(a.getClass, a.getClass.getMethods.toList.map(ClassMethod(m.erasure.getName, _)))
+  def create[T <: AnyRef](a: T)(implicit m: ClassManifest[T]): ScalaClassMethods = {
+    new ScalaClassMethods(a.getClass, a.getClass.getMethods.toList.map(ClassMethod(m.erasure.getName, _)))
   }
   private val MethodNamePattern = """(.*)\[(.*)\]""".r
   private[methods] def extractNameAndTypes(methodName: String) = {
@@ -72,7 +81,7 @@ object ScalaMethods {
  */
 trait ScalaMethodsFactory {
   implicit def toMethods[T <: AnyRef](a: T)(implicit m: ClassManifest[T]) = ScalaMethods.create(a)
-  implicit def toScalaMethods(m: ScalaMethod) = new ScalaMethods(m.declaringClass, List(m))
+  implicit def toScalaClassMethods(m: ScalaMethod) = new ScalaClassMethods(m.declaringClass, List(m))
   import org.scalacheck._
   def variable [A](name: String)(implicit m: ClassManifest[A], arb: Arbitrary[A], params: Gen.Params) = new ArbitraryVariable(name)(m, arb, params)
   def constant[A](name: String, v: A)(implicit m: ClassManifest[A]) = new Constant(name, v)(m)
