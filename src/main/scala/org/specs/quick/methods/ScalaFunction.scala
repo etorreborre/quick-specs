6922018251fa289a63982e62dd4a634a531abafb
etorreborre <etorreborre@yahoo.com>
2010-07-16 09:11:44 +1000

    starting introducing typed functions

--- src/main/scala/org/specs/quick/methods/ScalaFunction.scala
+++ src/main/scala/org/specs/quick/methods/ScalaFunction.scala
@@ -11,3 +11,12 @@ trait ScalaFunction {
   def parameterTypes: Seq[String]
   def apply(values: Any*): Any
 }
+
+//class ScalaTypedFunction[a1, b](aName: String, function: a1 => b, params: Seq[String], aReturnType: String) 
+//   extends ScalaFunction with Function1[a1, b] {
+//  import Function._
+//  def name: String = aName
+//  def returnType = aReturnType
+//  def parameterTypes: Seq[String] = params
+//  def apply(values: a1): b = function.apply(values) 
+//}
